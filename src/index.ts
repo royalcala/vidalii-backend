aba030082cf1582df3bc75a225f7e6da7e5a5621
Roy Alcala Ortiz <alcala.rao@gmail.com>
2021-03-08 18:43:27 -0600

    ..

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import VidaliiService from "./vidalii";
 export { VidaliiService }
 export type { Context } from "./vidalii.server";
 export * as orm from './vidalii.orm'
+export * as val from './vidalii.orm.validator'
