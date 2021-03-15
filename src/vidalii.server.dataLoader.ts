6d3aa9821917c83e05e0b6a8eadbe8e4c5c53072
Roy Alcala Ortiz <alcala.rao@gmail.com>
2021-03-15 10:57:35 -0600

    with composition

---
+++ src/vidalii.server.dataLoader.ts
@@ -0,0 +1,24 @@
+import { AnyEntity, EntityName } from "@mikro-orm/core"
+import DataLoader from "dataloader"
+import { Context } from "."
+
+export function getDataLoader<T extends AnyEntity<T>>(entity: EntityName<T>, nestedField: string, pk: keyof T, context: Context): DataLoader<any, T, any> {
+    if (!context.dataLoader.has(nestedField)) {
+        context.dataLoader.set(
+            nestedField,
+            new DataLoader(async (ids: string[]) => {
+                const childDocs = await context.em.find(entity as any, { [pk]: { $in: ids } })
+                const map = {}
+                for (let index = 0; index < childDocs.length; index++) {
+                    let childDoc = childDocs[index]
+                    map[
+                        childDoc[pk as string]
+                    ] = childDoc
+                }
+                return ids.map(key => map[key] || [])
+            })
+        )
+
+    }
+    return context.dataLoader.get(nestedField)
+}
